c72bd73a198bf0b81fb20ab766e65a6e91e238be
Ricardo Vega <github@noctusoft.com>
2017-04-10 23:58:40 -0500

    Initial checkin with addition of AdventureWorks edmx file and initial executions.  Debugging is not working properly.

---
+++ Console.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace RzDb.CodeGen
+{
+    class ConsoleHarness
+    {
+        static void Main(string[] args)
+        {
+            var edmxFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase) + @"\Resources\AW.edmx";
+            var outputPath = Path.GetTempPath() + "RzDbCodeGen\\";
+            if (!Directory.Exists(outputPath)) Directory.CreateDirectory(outputPath);
+            // Code that goes into RzDb.CodeGenerations.tt file starts here --- 
+
+            new EdmxGenDemoGenerator(edmxFile, outputPath).ProcessTemplate();
+
+            // End of the Code that goes into RzDb.CodeGenerations.tt 
+            Process.Start(outputPath);
+        }
+    }
+}
